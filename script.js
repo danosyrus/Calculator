875db95e9ad3e28ae3625ab9f2a3853b3b1d7862
Dano Syrus <92238288+danosyrus@users.noreply.github.com>
2024-04-04 12:01:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function indigo(){
+    document.getElementById("div").style.backgroundColor = "#7289DA";
+    document.getElementById("div").style.color = "white";
+}
+
+function cyan(){
+    document.getElementById("div").style.backgroundColor = "#00FFFF";
+    document.getElementById("div").style.color = "black";
+}
+
+function teal(){
+    document.getElementById("div").style.backgroundColor = "#008080";
+    document.getElementById("div").style.color = "white";
+}
